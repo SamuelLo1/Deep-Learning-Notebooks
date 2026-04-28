0467770f33a0ce017aa345e0727e8b7ff5fc962c
SamuelLo <samuello300@gmail.com>
2026-04-27 21:06:02 -0700

    fix: dimensions for forward pass of the ResUnet

--- project2/ResUNet.py
+++ project2/ResUNet.py
@@ -165,20 +165,25 @@ class ConditionalUnet(nn.Module):
         # out = self.outblock(torch.cat((up2, down0), dim = 1))
         # ==================================================== #
 
-        down0 = self.init_conv(x)
-        print("down0 shape: ", down0.shape)
-        down1 = self.downblock1(self.fusion1(down0, temb2, cemb2))
-        print("down1 shape: ", down1.shape)
-        down2 = self.downblock2(self.fusion2(down1, temb1, cemb1))
-        print("down2 shape: ", down2.shape)
-        vec = self.to_vec(down2)
-        up0 = self.upblock0(vec)
-        print("up0 shape: ", up0.shape)
-        up1 = self.upblock1(up0, self.fusion3(down2, temb2, cemb2))
-        print("up1 shape: ", up1.shape)
-        up2 = self.upblock2(up1, self.fusion4(down1, temb2, cemb2))
-        print("up2 shape: ", up2.shape)
-        out = self.outblock(torch.cat((up2, down0), dim = 1))
-        print("out shape: ", out.shape)
+  
+        down0 = self.init_conv(x)                                 
+    
+        down1 = self.downblock1(down0)                            
+        skip1 = self.fusion1(down1, temb2, cemb2)                 
+        
+        down2 = self.downblock2(skip1)                           
+        skip2 = self.fusion2(down2, temb1, cemb1)                 
+        
+        vec = self.to_vec(skip2)                                 
 
+        up0 = self.upblock0(vec)                                 
+        
+        up1 = self.upblock1(up0, skip2)                           
+        up1_fused = self.fusion3(up1, temb2, cemb2)              
+        
+        up2 = self.upblock2(up1_fused, skip1)                     
+        up2_fused = self.fusion4(up2, temb2, cemb2)             
+        
+        out = self.outblock(torch.cat((up2_fused, down0), dim=1))
+        
         return out
